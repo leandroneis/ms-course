55d3d267e667534e06dac983237c447d09537867
Leandro Gustavo Neis <lgn200@gmail.com>
2024-03-29 22:49:25 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,14 +1,30 @@
 package com.devsuperior.hrpayroll.services;
 
+import com.devsuperior.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.hrpayroll.entities.Payment;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
 	
 	public Payment getPayment(Long workerId , int days) {
-		return new Payment("Bob",200.0,days);
-	}
+        Map<String,String> uriVariables =  new HashMap<>();
+        uriVariables.put("id",workerId.toString());
 
+        Worker worker = restTemplate.getForObject(workerHost+ "/workers/{id}",Worker.class,uriVariables);
+		return new Payment(worker.getName(),worker.getDailyIncome(),days);
+	}
 }
