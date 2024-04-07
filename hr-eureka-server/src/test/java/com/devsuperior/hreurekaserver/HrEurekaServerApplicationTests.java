fcf9d137a8fa3c736db553a1068b715006845eb4
Leandro <lgn200@gmail.com>
2024-04-07 20:29:53 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/devsuperior/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.devsuperior.hreurekaserver;
+
+package com.devsuperior.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
